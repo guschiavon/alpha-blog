b34a6e99ffcb97261e025ddf79bdee946449ec3b
Gus <gustavo.schiavon@gmail.com>
2020-10-04 18:48:55 -0300

    establish create and display action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,25 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
-
-  # GET /articles
-  # GET /articles.json
+  
   def index
-    @articles = Article.all # Instance variables allow us to use ERB on the view to render data dynamically
+    @articles = Article.all # Instance variables allow us to use ERB on the view to render data dynamically; they are bound to the 
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
   def show
     @article = Article.find(params[:id])
   end
 
-  
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description)) # require the top level key of 'article' (the database structure) and permit the title and description to be available to create the new article
+    if @article.save # saves the record on the database
+      flash[:notice] = "Congrats! Your article has been created"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+end
