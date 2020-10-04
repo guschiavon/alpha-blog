a5ac77de34a28333a000ef761c716e14b4f1d0c5
Gus <gustavo.schiavon@gmail.com>
2020-10-04 19:05:35 -0300

    implement the update action for articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id]) # in order to populate the article view for edit, we call the same method of 'show' which pulls the data from the database
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description)) # require the top level key of 'article' (the database structure) and permit the title and description to be available to create the new article
     if @article.save # saves the record on the database
@@ -19,6 +23,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article has been updated."
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
