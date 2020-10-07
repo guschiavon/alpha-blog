75260f8f918d8f77a1538f38b108c01f9e5648f9
Gus <gustavo.schiavon@gmail.com>
2020-10-07 17:55:23 -0300

    Implement Bootstrap and work on basic styles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 6, maximum: 100 } # the entry needs to have a title which is between 6 and 100 characters in order to be saved as a database record
   validates :description, presence: true, length: { minimum: 20, maximum: 500 } # the entry needs to have a description between 20 & 500 words in order to be saved as a database record
+  
 end
