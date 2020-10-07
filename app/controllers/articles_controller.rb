07a762189495bb80ba313af57a897fccd227da09
Gus <gustavo.schiavon@gmail.com>
2020-10-07 10:34:13 -0300

    refactor code into methods and callbacks

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :destroy, :update]
   
+
   def index
     @articles = Article.all # Instance variables allow us to use ERB on the view to render data dynamically; they are bound to the 
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -13,12 +14,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id]) # in order to populate the article view for edit, we call the same method of 'show' which pulls the data from the database
-    
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description)) # require the top level key of 'article' (the database structure) and permit the title and description to be available to create the new article
+    @article = Article.new(article_params) # require the top level key of 'article' (the database structure) and permit the title and description to be available to create the new article
     if @article.save # saves the record on the database
       flash[:notice] = "Congrats! Your article has been created"
       redirect_to @article
@@ -28,8 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article has been updated."
       redirect_to @article
     else
@@ -37,10 +35,19 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def destroy
-    @article = Article.find(params[:id])
+  def destroy    
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id]) # in order to populate the article view for edit, we call the same method which pulls the data from the database
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
