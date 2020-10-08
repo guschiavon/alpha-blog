c74464d0fd7385d6b63ae91ec886432a46f3bf4b
Gus <gustavo.schiavon@gmail.com>
2020-10-08 11:21:58 -0300

    add one-to-many association between user and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_205607) do
+ActiveRecord::Schema.define(version: 2020_10_08_141010) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_10_07_205607) do
     t.string "author"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
