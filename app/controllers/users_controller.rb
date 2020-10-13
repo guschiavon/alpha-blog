0559679bb779704a3f3a1ea1a3b3d367dd04faa5
Gus <gustavo.schiavon@gmail.com>
2020-10-13 19:14:30 -0300

    add helper for User login/logout display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save 
+    if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome #{@user.username}!"
       redirect_to @user
     else
