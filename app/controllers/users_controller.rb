03fcbad658fc4c15725af0b5815a1cae00ff4501
Gus <gustavo.schiavon@gmail.com>
2020-10-17 00:28:14 -0300

    add admin features to delete users and articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,9 +37,11 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
-    flash[:notice] = "Your account has been deleted... :/"
-    redirect_to root_path
+    
+    session[:user_id] = nil if @user == current_user
+    
+    flash[:notice] = "Account has been deleted... :/"
+    redirect_to users_path
   end
 
 
@@ -54,7 +56,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You can only edit or delete your own profile!"
       redirect_to @user
     end
