6a450e89b44d4a4bceb78e61ee3ddd287ce4155c
Gus <gustavo.schiavon@gmail.com>
2020-11-06 09:28:35 -0300

    add activestorage for image uploading

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,11 @@ class ArticlesController < ApplicationController
   before_action :require_user, except: [:show, :index]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   
+  # article.cover_picture.attach(params[:cover_picture]) # Where do I call this for the image??
+  # article.cover_picture.attached?
+
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 5)
+    @articles = Article.paginate(page: params[:page], per_page: 6)
   end
 
   def show
@@ -47,10 +50,12 @@ class ArticlesController < ApplicationController
   def set_article
     @article = Article.find(params[:id]) # in order to populate the article view for edit, we call the same method which pulls the data from the database
   end
-
+  
   def article_params
-    params.require(:article).permit(:title, :description, :user_id)
+    params.require(:article).permit(:title, :description, :cover_picture, :user_id)
+    
   end
+  
 
   def require_same_user
     if current_user != @article.user && !current_user.admin?
